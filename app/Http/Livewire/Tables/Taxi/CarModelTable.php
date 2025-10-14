f05fbfe73cebaf64354f582fc5b960131a6f0e76
hanna0210 <hannadubose41@gmail.com>
2025-10-14 11:02:29 +0300

    add car model deletion button

--- app/Http/Livewire/Tables/Taxi/CarModelTable.php
+++ app/Http/Livewire/Tables/Taxi/CarModelTable.php
@@ -5,12 +5,21 @@ namespace App\Http\Livewire\Tables\Taxi;
 use App\Http\Livewire\Tables\BaseTableComponent;
 use App\Models\CarModel;
 use Kdion4891\LaravelLivewireTables\Column;
+use Exception;
 
 class CarModelTable extends BaseTableComponent
 {
 
     public $model = CarModel::class;
     public $header_view = 'components.buttons.new';
+
+    protected $listeners = [
+        'activateModel',
+        'deactivateModel',
+        'deleteModel',
+        'filterUsers',
+        'refreshTable' => '$refresh',
+    ];
 
     public function query()
     {
@@ -24,6 +33,18 @@ class CarModelTable extends BaseTableComponent
             Column::make(__('Make'),'car_make.name')->searchable(),
             Column::make(__('Name'),'name')->searchable(),
             Column::make(__('Actions'))->view('components.buttons.edit'),
+            Column::make(__('Delete'))->view('components.buttons.delete'),
         ];
+    }
+
+    public function deleteModel()
+    {
+        try {
+            $this->isDemo();
+            $this->selectedModel->delete();
+            $this->showSuccessAlert(__("Car Model deleted successfully!"));
+        } catch (Exception $error) {
+            $this->showErrorAlert($error->getMessage() ?? __("Deletion failed!"));
+        }
     }
 }

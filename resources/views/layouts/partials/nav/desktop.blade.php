bc8b62a3238649504367e84095c340b517e3fb79
hanna0210 <hannadubose41@gmail.com>
2025-10-14 10:38:30 +0300

    fix

--- resources/views/layouts/partials/nav/desktop.blade.php
+++ resources/views/layouts/partials/nav/desktop.blade.php
@@ -5,7 +5,7 @@
     <div class="py-4 text-gray-500 dark:text-gray-400">
         <a class="flex items-center space-x-4 text-lg font-bold text-gray-100 ltr:ml-6 rtl:mr-6"
             href="{{ route('dashboard') }}">
-            <img src="{{ appLogo() }}" class="w-16 h-16 rounded" />
+            <img src="{{ appLogo() }}" class="w-16 h-10 rounded object-contain" />
             <div class="ltr:pl-6 rtl:pr-6">
                 <p>{{ setting('websiteName', env('APP_NAME')) }}</p>
                 <!-- <p class="text-xs text-gray-200">{{ __('version') }} {{ setting('appVerison', '1.0.0') }}</p> -->

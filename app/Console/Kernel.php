bc6aeadd77669310abf65626b1d29b75bd4852d5
hanna0210 <hannadubose41@gmail.com>
2025-10-27 22:25:31 -0700

    f

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->        command('inspire')->hourly();
-        //         //         //         //         //         //         //         //         //         //         //         // $schedule->command('queue:work')->everyMinute();
+        //         //         //         //         //         //         //         //         //         //         //         //         // $schedule->command('queue:work')->everyMinute();
         $schedule->command('queue:work', [
             '--max-time' => 120
         ])->withoutOverlapping();
